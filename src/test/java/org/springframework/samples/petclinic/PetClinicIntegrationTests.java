ff156abd339dd00b886b27446a7f248a38843dd0
Sakshiambhore <160828531+Sakshiambhore@users.noreply.github.com>
2024-07-17 11:54:15 +0530

    Update PetClinicIntegrationTests.java
    fixed test cases

--- src/test/java/org/springframework/samples/petclinic/PetClinicIntegrationTests.java
+++ src/test/java/org/springframework/samples/petclinic/PetClinicIntegrationTests.java
@@ -55,10 +55,6 @@ public class PetClinicIntegrationTests {
 		ResponseEntity<String> result = template.exchange(RequestEntity.get("/owners/1").build(), String.class);
 		assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
-    @Test
-	void contextloads() {
-	 assertEquals(2,2);
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(PetClinicApplication.class, args);
 	}
